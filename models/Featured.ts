d338028474e51b055ffb3e118ca44e12ff48f878
John Corrigan <jcorrigan322@gmail.com>
2022-12-04 18:09:31 -0500

    fix deployment

--- models/Featured.ts
+++ models/Featured.ts
@@ -1,22 +1,30 @@
-export default class Featured  {
-    
-    id: number;
-    title: string;
-    mediaType: string;
-    posterPath: string;
-    backdropPath: string;
-    voteAverage: number;
-    overview: string;
-    releaseDate: string;
+export default class FeaturedMovie {
+  id: number;
+  title: string;
+  mediaType: string;
+  posterPath: string;
+  backdropPath: string;
+  voteAverage: number;
+  overview: string;
+  releaseDate: string;
 
-    constructor(id: number, title: string, mediaType: string, posterPath: string, backdropPath: string, voteAverage: number, overview: string, releaseDate: string) {
-        this.id = id;
-        this.title = title;
-        this.mediaType = mediaType;
-        this.posterPath = `http://image.tmdb.org/t/p/w500${posterPath}`;
-        this.backdropPath = `http://image.tmdb.org/t/p/original${backdropPath}`;
-        this.voteAverage = voteAverage;
-        this.overview = overview;
-        this.releaseDate = releaseDate;
-    }
+  constructor(
+    id: number,
+    title: string,
+    mediaType: string,
+    posterPath: string,
+    backdropPath: string,
+    voteAverage: number,
+    overview: string,
+    releaseDate: string
+  ) {
+    this.id = id;
+    this.title = title;
+    this.mediaType = mediaType;
+    this.posterPath = `http://image.tmdb.org/t/p/w500${posterPath}`;
+    this.backdropPath = `http://image.tmdb.org/t/p/original${backdropPath}`;
+    this.voteAverage = voteAverage;
+    this.overview = overview;
+    this.releaseDate = releaseDate;
+  }
 }
