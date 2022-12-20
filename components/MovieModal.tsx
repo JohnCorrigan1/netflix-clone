552e7b07ff294d7a50882427491a74680ab1d953
John Corrigan <jcorrigan322@gmail.com>
2022-12-19 19:48:34 -0500

    movie modal

--- components/MovieModal.tsx
+++ components/MovieModal.tsx
@@ -11,6 +11,7 @@ const MovieModal: React.FC<{ open: boolean, movie: FeaturedMovie | null }> = (pr
     currentContext.setIsModalOpen(currentContext.isOpen);
   }
 
+  const releaseYear = props.movie?.releaseDate.split('-')[0];
 
 if (!props.open) return null
 
@@ -18,20 +19,23 @@ return (
 
 <>
   <div className='modal bg-black bg-opacity-70 fixed top-0 right-0 bottom-0 left-0 z-50' ></div>
-    <div className='bg-white flex flex-col gap-10 items-center p-12 rounded-xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 max-w-lg z-50'>
+    <div className='bg-zinc-900 text-zinc-200 flex flex-col items-center rounded-xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 max-w-3xl overflow-y-scroll scrollbar-hide h-5/6 z-50'>
       <div className="hover:bg-zinc-400 rounded-full bg-opacity-50 active:scale-95 absolute top-3 right-3"
       onClick={closeHandler}
       >
       <Image src="/close.svg" height={40} width={40} alt="X" />
+      
       </div>
-      <h1 className='text-2xl font-bold'>{props.movie?.title}</h1>
-      <div>
-        <Image src={props.movie!.backdropPath} height={400} width={400} alt={props.movie!.title}/>
+
+      {/* <h1 className='text-2xl font-bold mt-5 flex'>{props.movie?.title}</h1> */}
+      <div className="w-full rounded-xl">
+        <Image src={props.movie!.backdropPath} height={800} width={800} alt={props.movie!.title}/>
       </div>
-    <div className='flex flex-col justify-center items-center gap-10'>
-      <button className='rounded-lg w-fit p-3 bg-cyan-500 hover:bg-cyan-600 shadow-md shadow-slate-500 text-xl'>Btn 1</button>
-      <button className=' hover:bg-rose-600 text-xl rounded-lg bg-rose-500 w-fit p-3 shadow-md shadow-slate-500'>Btn 2</button>
-    </div>
+      <div className="flex w-full">
+      <h2 className=" ml-16 font-semibold">{releaseYear}</h2>
+        <h2 className=" ml-16 font-semibold">{props.movie?.mediaType}</h2>
+
+      </div>
   </div>
 </> 
 )
