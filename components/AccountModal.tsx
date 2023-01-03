6aed4c817cc677eb2ee0ba92595173ed51192020
John Corrigan <93392001+JohnCorrigan1@users.noreply.github.com>
2023-01-03 15:57:50 +0000

    account modal

--- components/AccountModal.tsx
+++ components/AccountModal.tsx
@@ -1,17 +1,25 @@
-const AccountModal: React.FC = () => {
+import { Dispatch, SetStateAction } from "react"
+import Image from "next/image"
+
+const AccountModal: React.FC<{ isOpen: boolean, currentUser: string, setIsOpen: Dispatch<SetStateAction<boolean>> }> = (props) => {
 
     const closeHandler = () => {
-        console.log("clicked")
+        props.setIsOpen(false)
     }
+
+if(!props.isOpen)
+    return null
 
 return (
     <>
     <div onClick={closeHandler} className='modal bg-black bg-opacity-70 fixed top-0 right-0 bottom-0 left-0 z-[2000]' ></div>
       <div className='bg-zinc-900 text-zinc-200 flex flex-col items-center rounded-xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 max-w-3xl overflow-y-scroll scrollbar-hide h-5/6 z-[2001]'>
-        <div className="hover:bg-zinc-400 rounded-full bg-opacity-50 active:scale-95 absolute top-3 right-3"
-        onClick={closeHandler}
-        >
-        </div>
+      <div className="hover:bg-zinc-400 rounded-full bg-opacity-50 active:scale-95 absolute top-3 right-3"
+      onClick={closeHandler}
+      >
+      <Image src="/close.svg" height={40} width={40} alt="X" />
+      
+      </div>
     </div>
   </> )
 }
--- components/NewAccount.tsx
+++ components/NewAccount.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 
-const NewAccount: React.FC = () => {
+const NewAccount: React.FC<{ isOpen: boolean, setIsOpen: Dispatch<SetStateAction<boolean>> }> = (props) => {
+
+    const modalHandler = () => {
+        props.setIsOpen(true)
+    }
+
     return (
-        <div className="bg-zinc-500 flex flex-col w-32 h-32 justify-center items-center hover:bg-zinc-400 cursor-pointer">
+        <div className="bg-zinc-500 flex flex-col w-32 h-32 justify-center items-center hover:bg-zinc-400 cursor-pointer rounded-lg"
+        onClick={modalHandler}>
             <Image src="/add.svg" height={75} width={75} alt="anon" />
-            <p className="text-zinc-900 font-bold text-2xl">Add Account</p>
+            <p className="text-zinc-900 font-bold text-2xl">Create</p>
         </div>
 
     )
